966eb960e3bb18936f16202df98d74c700086ae9
victor-aunon <hola@victoraunon.com>
2023-01-06 16:07:00 +0100

    Added book_list view

--- bookr/bookr/urls.py
+++ bookr/bookr/urls.py
@@ -21,5 +21,6 @@ import reviews.views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", reviews.views.index),
-    path("book-search", reviews.views.book_search)
+    path("book-search/", reviews.views.book_search),
+    path("books/", reviews.views.book_list, name="book_list")
 ]
